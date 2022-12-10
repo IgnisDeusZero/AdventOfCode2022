d50a0c15a484aeb0a7ef9d3aac7766d495101d21
Ignis Deus <ignisdeus2048@gmail.com>
2022-12-10 12:55:23 +0700

    Day 10 done

---
+++ AdventOfCode2022/Solutions/Day10.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode2022.Solutions
+{
+    public class Day10 : SolutionBase
+    {
+        private readonly int[] cycles = new[] { 20, 60, 100, 140, 180, 220 };
+        public Day10() : base("./Inputs/Day10.txt")
+        {
+        }
+
+        public override string Part1()
+        {
+            return Input
+                .Replace("\r\n", "/")
+                .Replace("\n", "/")
+                .Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries)
+                .SelectMany(x => x == "noop" ? new[] { 0 } : new[] { 0, int.Parse(x.Remove(0, 5)) })
+                .Select((x, i) => (x, i))
+                .Aggregate(new[] { 1, 0 }, (sig, c) =>
+                {
+                    if (cycles.Any(x => x == (c.i + 1)))
+                    {
+                        sig[1] += sig[0] * (c.i + 1);
+                    }
+                    sig[0] += c.x;
+                    return sig;
+                })[1]
+                .ToString();
+        }
+
+        public override string Part2()
+        {
+            return Input
+                .Replace("\r\n", "/")
+                .Replace("\n", "/")
+                .Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries)
+                .SelectMany(x => x == "noop" ? new[] { 0 } : new[] { 0, int.Parse(x.Remove(0, 5)) })
+                .Select((x, i) => (x, i))
+                .Aggregate(new 
+                    { 
+                        Signal = new[] { 1 }, 
+                        Builder = new System.Text.StringBuilder()
+                    }, 
+                    (state, c) =>
+                    {
+                        state.Builder.Append(c.i % 40 == 0 ? "\n" : "");
+                        state.Builder.Append(Math.Abs(state.Signal[0] - (c.i % 40)) <= 1 ? "#" : ".");
+                        state.Signal[0] += c.x;
+                        return state;
+                    })
+                .Builder
+                .ToString();
+        }
+    }
+}
